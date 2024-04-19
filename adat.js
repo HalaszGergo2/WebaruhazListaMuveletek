780b6e7d6780aee44a819658261d0326d5e658cf
HalaszGergo2 <xlomox12@gmail.com>
2024-04-19 12:06:04 +0200

    html kezdet, markalista

---
+++ adat.js
@@ -0,0 +1,22 @@
+export const termekLISTA=[
+    {
+        marka: "Nike",
+        nem: "férfi",
+        ar: 15000
+    },
+    {
+        marka: "Adidas",
+        nem: "női",
+        ar: 10000
+    },
+    {
+        marka: "Ralph Lauren",
+        nem: "férfi",
+        ar: 25000
+    },
+    {
+        marka: "Shein",
+        nem: "női",
+        ar: 1500
+    }
+]
