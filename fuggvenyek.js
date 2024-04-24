cc729cd354f71133f662797f5dd08b2679a0aa89
HalaszGergo2 <xlomox12@gmail.com>
2024-04-24 15:40:06 +0200

    kosarhozad, torol mukodik

---
+++ fuggvenyek.js
@@ -0,0 +1,9 @@
+export function kosarbaRak(lista1, lista2, id) {
+  lista2.push(lista1[id]);
+ 
+}
+export function torol(lista, id){
+  lista.splice(id, 1)
+}
+//console.log(lista1[id])
+// lista2.push(lista1[id])
--- htmlOsszeallit.js
+++ htmlOsszeallit.js
@@ -1,7 +1,7 @@
-export function htmlCardOsszeallit(lista){
-    let txt = "<div class='row'>";
-    for(let i = 0; i < lista.length; i++){
-        txt += `
+export function htmlCardOsszeallit(lista) {
+  let txt = "<div class='row'>";
+  for (let i = 0; i < lista.length; i++) {
+    txt += `
         <div class="col-lg-3 mb-3">
             <div class="card">
                 <div class="card-header">
@@ -12,36 +12,36 @@ export function htmlCardOsszeallit(lista){
                     <p class="card-text">${lista[i].nem}</p>
                 </div>
                 <div class="card-footer">
-                    <p>Ár: ${lista[i].ar} Ft <button class="kosargomb btn btn-primary" style="float: right;">Kosárba</button></p>
+                    <p>Ár: ${lista[i].ar} Ft <button id="${i}" class="kosargomb btn btn-primary" style="float: right;">Kosárba</button></p>
                 </div>
             </div>
         </div>`;
-    }
-    txt += "</div>";
-    return txt;
+  }
+  txt += "</div>";
+  return txt;
 }
 
-export function htmlKosarOsszeallit(lista){
-    let txt = "<div class='tabla row'>";
-    txt += "<table class='table table-striped col-lg-4'>"
-    txt += "<h1>Kosár:</h1>"
-    txt += "<thead><tr><th>Márka:</th><th>Nem:</th><th>Ár(Ft):</th><th></th></tr></thead>"
-        txt += "<tbody>"
-    lista.forEach((elem, index) => {
-        txt += `<tr>`
-        for (const kulcs in elem) {
-             txt += `<td>${elem[kulcs]}</td>`
-        }
-        txt +=`<td class='torol' id=${index}>🗑️</td>`
-        
-        txt += `</tr>`
-    })
-    txt += "</tbody></table></div>"
-    return txt
-    };
-    
-    
+export function htmlKosarOsszeallit(lista) {
+  let txt = "<div class='tabla row'>";
+  txt += "<table class='table table-striped col-lg-4'>";
+  txt += "<h1>Kosár:</h1>";
+  txt +=
+    "<thead><tr><th>Márka:</th><th>Nem:</th><th>Ár(Ft):</th><th></th></tr></thead>";
+  txt += "<tbody>";
 
-export function megjelenit(txt, divELEM){
-    divELEM.html(txt);
+  lista.forEach((elem, index) => {
+    txt += `<tr>`;
+    for (const kulcs in elem) {
+      txt += `<td>${elem[kulcs]}</td>`;
+    }
+    txt += `<td class='torol' id=${index}>🗑️</td>`;
+
+    txt += `</tr>`;
+  });
+  txt += "</tbody></table></div>";
+  return txt;
+}
+
+export function megjelenit(txt, divELEM) {
+  divELEM.html(txt);
 }
