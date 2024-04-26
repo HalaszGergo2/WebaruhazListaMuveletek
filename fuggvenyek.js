84b9e43762d9cc5ab7a32187148eaccb5532e56b
HalaszGergo2 <xlomox12@gmail.com>
2024-04-26 12:03:35 +0200

    Nagyjabol mukodik minden

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,24 +1,24 @@
 export function kosarbaRak(lista1, lista2, id) {
   lista2.push(lista1[id]);
- 
 }
-export function torol(lista, id){
-  lista.splice(id, 1)
-}
-
-export function szures(lista, keresoSzoveg){
-  lista.filter(function(termek){
-  return termek.marka.toUpperCase().includes(keresoSzoveg.toUpperCase())
-})
+export function torol(lista, id) {
+  lista.splice(id, 1);
 }
 
-export function rendez(lista, irany){
-  lista.sort(function(t1,t2){
-      let eredmeny = 1
-      if(t1.marka < t2.marka){
-          eredmeny = -1
-      }
-      return eredmeny*irany;
-  })
-  return lista
+export function szures(lista, keresoSzoveg) {
+  let szurt = lista.filter(function (termek) {
+    return termek.marka.toUpperCase().includes(keresoSzoveg.toUpperCase());
+  });
+  return szurt;
+}
+
+export function rendez(lista, irany) {
+  lista.sort(function (t1, t2) {
+    let eredmeny = 1;
+    if (t1.marka < t2.marka) {
+      eredmeny = -1;
+    }
+    return eredmeny * irany;
+  });
+  return lista;
 }
